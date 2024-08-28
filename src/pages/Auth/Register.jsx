757af1925e0901de01766047cbae33b0d6333ded
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-28 19:48:09 -0300

    Configuração inicial do redux e das requisições http
    Criação da função que irá configurar as requisições http, e configuração inicial do redux que irá compartilhar os dados de autenticação entee os componentes

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -7,8 +7,20 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
   };
 
   return (
@@ -16,10 +28,20 @@ const Register = () => {
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastre-se para interagir com outros usuários.</p>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <input type="password" placeholder="Confirmação de senha" />
+        <input type="text" placeholder="Nome" value={name || ''} onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder="E-mail" value={email || ''} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password || ''}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirmação de senha"
+          value={confirmPassword || ''}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <input type="submit" value="Cadastrar" />
       </form>
       <p>
---
+++ src/utils/config.js
@@ -0,0 +1,36 @@
+import { config } from 'dotenv';
+config();
+
+export const api = process.env('URL_API');
+export const uploads = process.env('URL_UPLOADS');
+
+export const requestConfig = (method, data, token = null, image = null) => {
+  let config;
+
+  if (image) {
+    config = {
+      method,
+      body: data,
+      headers: {},
+    };
+  } else if (method === 'DELETE' || data === null) {
+    config = {
+      method,
+      headers: {},
+    };
+  } else {
+    config = {
+      method,
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
