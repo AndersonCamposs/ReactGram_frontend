56ba0fc2ab70e451fc053f969f62b7f1c16e21aa
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-28 18:54:44 -0300

    Criação do formulário de registro e estilização global

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,7 +1,32 @@
 import './Auth.css';
 
+// COMPONENTS
+import { Link } from 'react-router-dom';
+
+// HOOKS
+import { useState, useEffect } from 'react';
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para interagir com outros usuários.</p>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirmação de senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
