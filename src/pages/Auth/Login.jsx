bbe069a932d613157d98cac4cd95c5e74ea41ebe
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-29 19:51:57 -0300

    Estruturação do formulário de login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,7 +1,43 @@
 import './Auth.css';
 
+// COMPONENTS
+import { Link } from 'react-router-dom';
+import Message from '../../components/Message';
+
+// HOOKS
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// REDUX
+
 const Login = () => {
-  return <div id="login">Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça login para ver o que há de novo.</p>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email || ''} />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ''}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui e registre-se.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
