b53c00982a173b13249a50e3bc14a9b971784e3d
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-30 20:16:03 -0300

    Configuração do service e slice do usuário para resgatar seus dados
    Agora os dados são resgatados o formulário da página de edição de perfil é atualizado é preenchido com estes dados que foram regatados.

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,6 +1,44 @@
 import './EditProfile.css';
 
+import { uploads } from '../../utils/config';
+
+// HOOKS
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// REDUX
+import { profile, resetMessage } from '../../slices/userSlice';
+
+// COMPONENTS
+import Message from '../../components/Message';
+
 const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  // STATES
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+  const [bio, setBio] = useState('');
+  const [previewImage, setPreviewImage] = useState('');
+
+  // LOAD USER DATA
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // FILL FORM WITH USER DATA
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio);
+    }
+  }, [user]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -11,19 +49,35 @@ const EditProfile = () => {
       <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você...</p>
       {/* PREVIEW DA IMAGEM */}
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" disabled />
+        <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ''} />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ''}
+          disabled
+        />
         <label>
           <span>Imagem do perfil:</span>
           <input type="file" />
         </label>
         <label>
           <span>Biografia:</span>
-          <input type="text" placeholder="Descrição do perfil" />
+          <input
+            type="text"
+            placeholder="Descrição do perfil"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio || ''}
+          />
         </label>
         <label>
           <span>Alterar senha:</span>
-          <input type="password" placeholder="Digite sua senha" />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ''}
+          />
         </label>
         <input type="submit" value="Atualizar" />
       </form>
--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+import userService from '../services/userService';
+
 const initialState = {
   user: {},
   error: false,
@@ -8,7 +10,14 @@ const initialState = {
   message: null,
 };
 
-// FUNCTIONS
+// GET USER DETAIL
+export const profile = createAsyncThunk('user/profil', async (user, thunkAPI) => {
+  const token = thunkAPI.getState().auth.user.token;
+
+  const data = await userService.profile(user, token);
+
+  return data;
+});
 
 export const userSlice = createSlice({
   name: 'user',
@@ -18,7 +27,20 @@ export const userSlice = createSlice({
       state.message = null;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(profile.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(profile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      });
+  },
 });
 
 export const { resetMessage } = userSlice.actions;
-export default userSlice.reducer;
+export default userSlice.reducer; // THIS EXPORT IS USED IN store.js
